75fc905cf7d28c47c570e3207031cdd157917320
Stanislav Kovtun <stanislav.kovtun@cpcs.ws>
2023-10-11 17:06:26 +0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
+import { Routes, Route } from 'react-router';
+
 import Main from './pages/Main'
+import Contacts from './pages/Contacts'
+import './App.css'
 
 function App() {
   return (
-    <Main />
-  )
+    <div>
+      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
