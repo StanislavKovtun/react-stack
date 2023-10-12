543bcc00417d00dafbf43d4499fad75eb2b88cf7
Stanislav Kovtun <sstanisslaw@gmail.com>
2023-10-12 16:25:47 +0300

    Add example of error handling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,17 @@
-import { Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import Header from "./Header"
 
 const Layout = () => {
+    
+    //TODO: example of error
+    //(null).toString();
+
+    // As result we get: 
+    //Oops!
+    //Sorry, something went wrong
+    //Cannot read properties of null (reading 'toString')
+
     return (
         <>
             <Header />
