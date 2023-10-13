3e1a93b8d724c7209252c493c711433d2c6566f9
Stanislav Kovtun <stanislav.kovtun@cpcs.ws>
2023-10-13 17:14:54 +0300

    Add tailwind classes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,13 @@
-
 function About() {
     return (
-        <h1>About</h1>
+        <>
+            <h1 className="mb-5">About</h1>
+            <form className="bg-slate-400 p-5 rounded w-fit flex flex-col gap-5 m-auto">
+                <h2 className="font-medium font-s text-xl">Form</h2>
+                <input className="block h-10 rounded"></input>
+                <button type="button" className="bg-slate-800 text-white">Click me!</button>
+            </form>
+        </>
     )
 }
 
