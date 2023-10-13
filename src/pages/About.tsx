d99b1c4b91d7f3cec6eba940f9d5c8e8bd2d17d5
Stanislav Kovtun <stanislav.kovtun@cpcs.ws>
2023-10-13 17:44:05 +0300

    Use apply in css module

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,11 @@
 import { SmileOutlined } from '@ant-design/icons';
+import styles from './About.module.scss'
 
 function About() {
     return (
         <>
-            <h1 className="mb-5">About</h1>
-            <form className="bg-slate-400 p-5 rounded w-fit flex flex-col gap-5 m-auto hover:bg-opacity-90">
+            <h1 className={styles.header}>About</h1>
+            <form className={styles.form}>
                 <h2 className="font-medium font-s text-xl">Form</h2>
                 <input className="block h-10 rounded"></input>
                 <button type="button" className="bg-slate-800 text-white">Click me!</button>
